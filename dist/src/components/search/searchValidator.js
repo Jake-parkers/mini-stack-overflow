6896253d1db7cd86e614c3618bf107cfe7bcb569
Jake-parkers <joshuakwaku@outlook.com>
2020-08-25 00:03:06 +0100

    removed dist folder

--- dist/src/components/search/searchValidator.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.searchValidator = void 0;
-const express_validator_1 = require("express-validator");
-exports.searchValidator = [
-    express_validator_1.query('type').exists().trim().escape(),
-    express_validator_1.query('query').notEmpty().exists().trim().escape()
-];
-//# sourceMappingURL=searchValidator.js.map
